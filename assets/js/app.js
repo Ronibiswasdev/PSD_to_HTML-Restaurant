c688de202f600f974046b9fd27a84c464d10807a
RoniBiswas <ronibiswas068@gmail.com>
2020-12-04 21:23:31 +0600

    add smooth scroll and change bootstrap bundle to bootstrap.min.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,28 @@
         //add sticky menu
         $(".header-area").sticky({ topSpacing: 0 });
 
+        //for smooth scroll
+        $('li.smooth-menu a').bind('click', function(event) {
+            var $anchor = $(this);
+            var headerH = '65';
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - headerH + "px"
+            }, 1200, 'easeInOutExpo');
+
+            event.preventDefault();
+        });
+
+        //jquery scrollspy
+        // $('body').scrollspy({
+        //     target: '.navbar',
+        //     offset: 50
+        // });
+
+
+
+
+
+
     });
 
     jQuery(window).load(function() {
